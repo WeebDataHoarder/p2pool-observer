0aa33c9dc1fb8fc67ff874ef9f65c71f8bf9167b
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2022-11-03 15:25:15 +0100

    Fix deterministic address generation

--- monero/address/address_test.go
+++ monero/address/address_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"filippo.io/edwards25519"
 	"git.gammaspectra.live/P2Pool/p2pool-observer/monero/crypto"
+	"git.gammaspectra.live/P2Pool/p2pool-observer/types"
 	"log"
 	"testing"
 )
@@ -12,6 +13,7 @@ import (
 var privateKey = edwards25519.NewScalar()
 
 var testAddress = FromBase58("42HEEF3NM9cHkJoPpDhNyJHuZ6DFhdtymCohF9CwP5KPM1Mp3eH2RVXCPRrxe4iWRogT7299R8PP7drGvThE8bHmRDq1qWp")
+var testAddress2 = FromBase58("4AQ3YkqG2XdWsPHEgrDGdyQLq1qMMGFqWTFJfrVQW99qPmCzZKvJqzxgf5342KC17o9bchfJcUzLhVW9QgNKTYUBLg876Gt")
 
 var ephemeralPubKey, _ = hex.DecodeString("20efc1310db960b0e8d22c8b85b3414fcaa1ed9aab40cf757321dd6099a62d5e")
 
@@ -30,5 +32,15 @@ func TestAddress(t *testing.T) {
 
 	if bytes.Compare(ephemeralPublicKey.Bytes(), ephemeralPubKey) != 0 {
 		t.Fatalf("ephemeral key mismatch, expected %s, got %s", hex.EncodeToString(ephemeralPubKey), hex.EncodeToString(ephemeralPublicKey.Bytes()))
+	}
+}
+
+var previousId, _ = types.HashFromString("d59abce89ce8131eba025988d1ea372937f2acf85b86b46993b80c4354563a8b")
+var detTxPriv, _ = types.HashFromString("10f3941fd50ca266d3350984004a804c887c36ec11620080fe0b7c4a2a208605")
+
+func TestDeterministic(t *testing.T) {
+	detTx := types.HashFromBytes(testAddress2.GetDeterministicTransactionPrivateKey(previousId).Bytes())
+	if detTx != detTxPriv {
+		t.Fatal()
 	}
 }
--- monero/crypto/random.go
+++ monero/crypto/random.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/binary"
 	"filippo.io/edwards25519"
@@ -14,6 +13,19 @@ import (
 // limit fits 15 times in 32 bytes (iow, 15 l is the highest multiple of l that fits in 32 bytes)
 var limit = []byte{0xe3, 0x6a, 0x67, 0x72, 0x8b, 0xce, 0x13, 0x29, 0x8f, 0x30, 0x82, 0x8c, 0x0b, 0xa4, 0x10, 0x39, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0}
 
+// less32 each input must be at least 32 bytes long
+func less32(a, b []byte) bool {
+	for n := 31; n >= 0; n-- {
+		if a[n] < b[n] {
+			return true
+		} else if a[n] > b[n] {
+			return false
+		}
+	}
+
+	return false
+}
+
 func RandomScalar() *edwards25519.Scalar {
 	buf := make([]byte, 32)
 	for {
@@ -21,12 +33,12 @@ func RandomScalar() *edwards25519.Scalar {
 			return nil
 		}
 
-		if bytes.Compare(buf, limit) > 0 {
+		if !less32(buf, limit) {
 			continue
 		}
 
 		scalar := BytesToScalar(buf)
-		if scalar.Equal(edwards25519.NewScalar()) == 0 {
+		if scalar.Equal(zeroScalar) == 0 {
 			return scalar
 		}
 	}
@@ -49,12 +61,12 @@ func DeterministicScalar(entropy []byte) *edwards25519.Scalar {
 		binary.LittleEndian.PutUint32(buf[len(entropy):], counter)
 		_, _ = h.Write(buf)
 		_ = h.Sum(hash[:0])
-		if bytes.Compare(hash[:types.HashSize], limit) > 0 {
+		if !less32(hash[:types.HashSize], limit) {
 			continue
 		}
 		scalar, _ = scalar.SetUniformBytes(hash)
 
-		if scalar.Equal(edwards25519.NewScalar()) == 0 {
+		if scalar.Equal(zeroScalar) == 0 {
 			return scalar
 		}
 	}
